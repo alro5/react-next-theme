10e37ab4c1b9cfae28f940205ad9c1f437561d95
Alexander Rothschild <ar@bolighed.dk>
2021-12-15 20:11:12 +0100

    Use UserProfile

--- pages/users/[uid].tsx
+++ pages/users/[uid].tsx
@@ -1,19 +1,10 @@
-import { useRouter } from "next/router";
-import { useGetUser } from "../../hooks/user";
+import { UserProfile } from "../../src/components/user-profile";
 
 const User = () => {
-  const router = useRouter()
-  const { uid } = router.query;
-
-  const { user, isLoading, isError } = useGetUser(uid as string);
-
-  if (isLoading) return <div>Loading...</div>
-  if (isError) return <div>Error</div>
-
   return (
     <div>
       <h1>User</h1>
-      {user.name}
+      <UserProfile />
     </div>
   )
 
--- src/components/user-profile.tsx
+++ src/components/user-profile.tsx
@@ -1,21 +1,26 @@
+import { useRouter } from 'next/router';
 import React from 'react';
-import { useUser } from '../../hooks/user';
+import { useGetUser } from '../../hooks/user';
 
 interface UserProfileProps {
 
 }
 
 export function UserProfile(props: UserProfileProps): JSX.Element {
-  const { user, isLoading, isError } = useUser();
+  const router = useRouter()
+  const { uid } = router.query;
+
+  const { user, isLoading, isError } = useGetUser(uid as string);
 
   if (isLoading) return <div>Loading...</div>
   if (isError) return <div>Error</div>
 
   return <div className="user-profile">
+    <img src="https://thiscatdoesnotexist.com/" alt="Cat" />
     <p>
       {user.name}<br />
       {user.phone} <br />
-      {user.address}
+      {user.address.street}
     </p>
   </div>
 }
